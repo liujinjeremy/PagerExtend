813e18ed178e76c57ef0e8586aee00fab4ceabed
liujin <wuxiongc@163.com>
2018-09-16 09:51:07 +0800

    add pagerAdapter

--- app/src/main/java/tech/threekilogram/viewpagerextendlib/MainActivity.java
+++ app/src/main/java/tech/threekilogram/viewpagerextendlib/MainActivity.java
@@ -1,17 +1,65 @@
 package tech.threekilogram.viewpagerextendlib;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 /**
  * @author liujin
  */
 public class MainActivity extends AppCompatActivity {
 
+      private FrameLayout    mContainer;
+      private NavigationView mNavigationView;
+      private DrawerLayout   mDrawer;
+
       @Override
       protected void onCreate ( Bundle savedInstanceState ) {
 
             super.onCreate( savedInstanceState );
             setContentView( R.layout.activity_main );
+            initView();
+      }
+
+      private void initView ( ) {
+
+            mContainer = findViewById( R.id.container );
+            mNavigationView = findViewById( R.id.navigationView );
+            mNavigationView.setNavigationItemSelectedListener(
+                new MainNavigationItemSelectedListener()
+            );
+            mDrawer = findViewById( R.id.drawer );
+      }
+
+      private void changeFragment ( Fragment fragment ) {
+
+            getSupportFragmentManager().beginTransaction()
+                                       .replace( R.id.container, fragment )
+                                       .commit();
+      }
+
+      private class MainNavigationItemSelectedListener implements OnNavigationItemSelectedListener {
+
+            @Override
+            public boolean onNavigationItemSelected ( @NonNull MenuItem item ) {
+
+                  switch( item.getItemId() ) {
+                        case R.id.menu00:
+                              changeFragment( MaxCountAdapterFragment.newInstance() );
+                              break;
+                        default:
+                              break;
+                  }
+
+                  mDrawer.closeDrawer( Gravity.START );
+                  return true;
+            }
       }
 }
---
+++ view-pager-extend/src/main/java/tech/threekilogram/viewpager/adapter/TypePagerKnife.java
@@ -0,0 +1,81 @@
+package tech.threekilogram.viewpager.adapter;
+
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+
+/**
+ * Created by LiuJin on 2018-01-01:13:23
+ *
+ * @author wuxio
+ */
+class TypePagerKnife {
+
+      private ArrayList<PagerItemInfo> mItemsInfo;
+
+      private SparseArray<ArrayList<PagerItemInfo>> mTypeItems;
+
+      private BaseTypePagerAdapter mPagerAdapter;
+
+      TypePagerKnife ( BaseTypePagerAdapter pagerAdapter ) {
+
+            mPagerAdapter = pagerAdapter;
+            mTypeItems = new SparseArray<>();
+      }
+
+      @SuppressWarnings("unchecked")
+      public PagerItemInfo getPagerItemInfo ( ViewGroup container, int position ) {
+
+            int type = mPagerAdapter.getViewType( position );
+            mItemsInfo = mTypeItems.get( type );
+            if( mItemsInfo == null ) {
+                  mItemsInfo = new ArrayList<>();
+                  mTypeItems.put( type, mItemsInfo );
+            }
+
+            PagerItemInfo info = null;
+            for( int i = 0; i < mItemsInfo.size(); i++ ) {
+                  PagerItemInfo infoNew = mItemsInfo.get( i );
+                  if( infoNew.getPosition() == -1 ) {
+                        info = infoNew;
+                        break;
+                  }
+            }
+            if( info == null ) {
+                  info = new PagerItemInfo();
+                  mItemsInfo.add( info );
+            }
+            info.setPosition( position );
+
+            Object data = mPagerAdapter.getData( position, mPagerAdapter.getViewType( position ) );
+            info.setData( data );
+            View view = info.getView();
+            if( view == null ) {
+                  view = mPagerAdapter.getView( container, position, type );
+                  info.setView( view );
+            }
+            mPagerAdapter.bindData( position, data, view, type );
+
+            return info;
+      }
+
+      public PagerItemInfo getPagerItem ( int position ) {
+
+            int type = mPagerAdapter.getViewType( position );
+            mItemsInfo = mTypeItems.get( type );
+            if( mItemsInfo == null ) {
+                  return null;
+            }
+
+            int size = mItemsInfo.size();
+            for( int i = 0; i < size; i++ ) {
+                  PagerItemInfo item = mItemsInfo.get( i );
+                  if( item.getPosition() == position ) {
+                        return item;
+                  }
+            }
+
+            return null;
+      }
+}
